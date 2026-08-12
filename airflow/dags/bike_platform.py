b284fc7b6cf972c4d02befe4c727b881e65903a4
krzysztof.kaluza <krzysztof.kaluza.96@gmail.com>
2026-08-12 11:56:01 +0200

    Setup airflow and DAG

---
+++ airflow/dags/bike_platform.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+from airflow.providers.standard.operators.bash import BashOperator
+
+from airflow import DAG
+
+with DAG(
+    dag_id="bike_platform",
+    start_date=(datetime(2026, 8, 1)),
+    schedule=None,
+    catchup=False,
+    tags=["data_platform"],
+) as dag:
+    start = BashOperator(task_id="start", bash_command="echo START")
+
+    docker_test = BashOperator(task_id="docker_test", bash_command="docker ps")
+
+    bronze = BashOperator(
+        task_id="bronze_layer",
+        bash_command=(
+            "docker exec spark /opt/spark/bin/spark-submit /opt/spark-apps/bronze.py"
+        ),
+    )
+
+    silver = BashOperator(
+        task_id="silver_layer",
+        bash_command=(
+            "docker exec spark /opt/spark/bin/spark-submit /opt/spark-apps/silver.py"
+        ),
+    )
+
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command="docker exec dbt bash -c 'cd /app/bike_rental && dbt run'",
+    )
+
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command="docker exec dbt bash -c 'cd /app/bike_rental && dbt test'",
+    )
+
+    dbt_docs_generate = BashOperator(
+        task_id="dbt_docs_generate",
+        bash_command="docker exec dbt bash -c 'cd /app/bike_rental && dbt docs generate'",
+    )
+
+    end = BashOperator(task_id="end", bash_command="echo END")
+
+    (
+        start
+        >> docker_test
+        >> bronze
+        >> silver
+        >> dbt_run
+        >> dbt_test
+        >> dbt_docs_generate
+        >> end
+    )
